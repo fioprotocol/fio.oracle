7ca986a811cc5e51f9c04e2a5adac05c2dcf8bd8
NikolasStelmah <nikolasstelmah@ukr.net>
2022-10-17 16:34:42 +0300

    Fix startup for FIO Testnet Servers

--- controller/util.js
+++ controller/util.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
+
+import fetch from "node-fetch";
 import config from '../config/config';
 const { curly } = require('node-libcurl')
 
 import { getLastProceededBlockNumberOnFioChain } from "./helpers";
 
+const fioHttpEndpoint = process.env.FIO_SERVER_URL_ACTION;
+
+//todo: switch curly onto fetch
 class UtilCtrl {
     constructor(){
     }
@@ -58,60 +63,60 @@ class UtilCtrl {
     }
 
     async getBalance(accountName) {
-      const data = await curly.post(process.env.FIO_SERVER_URL_ACTION+'v1/chain/get_account', {
+      const data = await curly.post(fioHttpEndpoint+'v1/chain/get_account', {
         postFields: JSON.stringify({ "account_name": accountName}),
         httpHeader: [
           'Content-Type: application/x-www-form-urlencoded',
         ],
       });
-      var balanceAmount = 0;
-      if (data.statusCode == 200) {
+      let balanceAmount = 0;
+      if (data.statusCode === 200) {
         const permission = data.data.permissions;
         const keyData = permission[0].required_auth.keys;
         const pubKey = keyData[0].key;
-        const balanceData = await curly.post(process.env.FIO_SERVER_URL_ACTION+'v1/chain/get_fio_balance', {
+        const balanceData = await curly.post(fioHttpEndpoint+'v1/chain/get_fio_balance', {
           postFields: JSON.stringify({ "fio_public_key": pubKey}),
           httpHeader: [
             'Content-Type: application/x-www-form-urlencoded',
           ],
         });
-        if(balanceData.statusCode == 200) {
+        if(balanceData.statusCode === 200) {
           balanceAmount = balanceData.data.balance;
         }
       }
       return balanceAmount;
     }
     async getOracleFee() {
-      const data = await curly.post(process.env.FIO_SERVER_URL_ACTION+'v1/chain/get_oracle_fees', {
+      const data = await curly.post(fioHttpEndpoint+'v1/chain/get_oracle_fees', {
         httpHeader: [
           'Content-Type: application/x-www-form-urlencoded',
         ],
       });
-      if(data.statusCode == 200) {
+      if(data.statusCode === 200) {
         const fee = data.data.oracle_fees[1].fee_amount;
         if (fee > 0) { console.log(true); return true; }
         else { console.log(false); return false; }
       }
     }
     async getFIOAddress(accountName) {
-      const data = await curly.post(process.env.FIO_SERVER_URL_ACTION+'v1/chain/get_account', {
+      const data = await curly.post(fioHttpEndpoint+'v1/chain/get_account', {
         postFields: JSON.stringify({ "account_name": accountName}),
         httpHeader: [
           'Content-Type: application/x-www-form-urlencoded',
         ],
       });
-      if (data.statusCode == 200) {
+      if (data.statusCode === 200) {
         const permission = data.data.permissions;
         const keyData = permission[0].required_auth.keys;
         const pubKey = keyData[0].key;
         var fio_address = "";
-        const addressData = await curly.post(process.env.FIO_SERVER_URL_ACTION+'v1/chain/get_fio_addresses', {
+        const addressData = await curly.post(fioHttpEndpoint+'v1/chain/get_fio_addresses', {
           postFields: JSON.stringify({ "fio_public_key": pubKey}),
           httpHeader: [
             'Content-Type: application/x-www-form-urlencoded',
           ],
         });
-        if(addressData.statusCode == 200) {
+        if (addressData.statusCode === 200) {
           const addresses = addressData.data.fio_addresses;
           fio_address = addresses[0].fio_address;
         }
@@ -119,28 +124,25 @@ class UtilCtrl {
       return fio_address;
     }
     async availCheck(fioName) {
-      const response = await curly.post(process.env.FIO_SERVER_URL_ACTION+'v1/chain/avail_check', {
+      const response = await curly.post(fioHttpEndpoint+'v1/chain/avail_check', {
         postFields: JSON.stringify({ "fio_name": fioName}),
         httpHeader: [
           'Content-Type: application/x-www-form-urlencoded',
         ],
       });
       var registered = 0;
-      if (response.statusCode == 200) {
+      if (response.statusCode === 200) {
         registered = response.data.is_registered;
       }
       return registered;
     }
-    async getInfo() {
-      const response = await curly.post(process.env.FIO_SERVER_URL_ACTION+'v1/chain/get_info', {
-        httpHeader: [
-          'Content-Type: application/x-www-form-urlencoded',
-        ],
-      });
-      var lastBlockNum = 0;
-      if (response.statusCode == 200) {
-        lastBlockNum = response.data.last_irreversible_block_num;
-      }
+
+    async getFioChainInfo() {
+      const fioChainInfo = await (await fetch(fioHttpEndpoint + 'v1/chain/get_info')).json()
+
+      let lastBlockNum = 0;
+      if (fioChainInfo.last_irreversible_block_num) lastBlockNum = fioChainInfo.last_irreversible_block_num;
+
       return lastBlockNum;
     }
 }
