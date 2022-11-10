ae26a7f09ca97e63b1f7f5d8f53c591b7533b481
NikolasStelmah <nikolasstelmah@ukr.net>
2022-11-10 15:54:08 +0200

    Fix Typo

--- controller/api/eth.js
+++ controller/api/eth.js
@@ -186,7 +186,7 @@ class EthCtrl {
                     jobIsRunningCacheKey: ORACLE_CACHE_KEYS.isWrapOnEthJobExecuting
                 })
             } else {
-                console.log(logPrefix + 'Oracle data is not walid, pls check .env and contract abi.');
+                console.log(logPrefix + 'Oracle data is not valid, pls check .env and contract abi.');
                 config.oracleCache.set(ORACLE_CACHE_KEYS.isWrapOnEthJobExecuting, false, 0);
             }
         } catch (err) {
