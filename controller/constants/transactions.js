7b369011dca4f7eb64bb780d267bd10d81a70473
Oleksii Trukhanov <trukhantech@gmail.com>
2024-10-07 19:54:52 +0300

    Add reverted by the evm error for chains txns.

--- controller/constants/transactions.js
+++ controller/constants/transactions.js
@@ -1,5 +1,6 @@
 export const NONCE_TOO_LOW_ERROR = 'nonce too low';
 export const ALREADY_KNOWN_TRANSACTION = 'already known';
 export const LOW_GAS_PRICE = 'was not mined';
+export const REVERTED_BY_THE_EVM = 'reverted by the EVM';
 
 export const MAX_RETRY_TRANSACTION_ATTEMPTS = 3;
