35c72802ace63a20d554a8c2bd8e9902f8d824a2
Oleksii Trukhanov <trukhantech@gmail.com>
2025-01-08 16:36:03 +0200

    DASH-1269 Make unique obtid. Add there tokenid to parse it on wrap status page.

--- controller/utils/general.js
+++ controller/utils/general.js
@@ -123,11 +123,3 @@ export const convertTimestampIntoMs = (timestamp) => {
   // If it's neither a valid timestamp nor a valid Date string
   throw new Error('Invalid input: Unable to convert timestamp into milliseconds.');
 };
-
-export const formatDateYYYYMMDD = (date) => {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // +1 because months are 0-indexed
-  const day = String(date.getDate()).padStart(2, '0');
-
-  return `${year}${month}${day}`;
-};
